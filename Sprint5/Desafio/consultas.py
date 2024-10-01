cf483899360eb1b3071851f9c1dda93a4e4c6cf4
Mayara Marques <164480318+mayaramog@users.noreply.github.com>
2024-10-01 13:46:54 +0000

    att readme sprint 5

--- Sprint5/Desafio/consultas.py
+++ Sprint5/Desafio/consultas.py
@@ -1,29 +1,31 @@
-import boto3
-import os
-from datetime import datetime
+import pandas as pd
 
-# Definindo variáveis
-aws_access_key_id = "APIKEY"
-aws_secret_access_key = "APISECRETACCESS"
-bucket_name = "tarefa3"
-raw_zone = "Raw"
-local = "Local"
-csv = "CSV"
-movies = "Movies"
-series = "Series"
-current_date = datetime.now().strftime("%Y/%m/%d")
-movies_file_path = "movies.csv"
-series_file_path = "series.csv"
+# Carregar o arquivo CSV
+arquivo_csv = 'bilheteria-diaria-obras-por-exibidoras-2024-08-s04.csv'
+df = pd.read_csv(arquivo_csv, delimiter=';')  # Usando o delimitador correto (;)
 
-# Configuração do cliente S3
-s3 = boto3.client("s3",
-                  aws_access_key_id=aws_access_key_id,
-                  aws_secret_access_key=aws_secret_access_key)
+# 1) Dois operadores lógicos
+consulta1 = df[(df['PUBLICO'] > 5) & (df['UF_SALA_COMPLEXO'] == 'BA')]
 
-# Upload dos arquivos CSV para o S3
-s3.upload_file(movies_file_path,
-               bucket_name,
-               os.path.join(raw_zone, local, csv, movies, current_date, "movies.csv"))
-s3.upload_file(series_file_path,
-               bucket_name,
-               os.path.join(raw_zone, local, csv, series, current_date, "series.csv"))
+
+# 2) Duas funções de agregação (soma e contagem)
+agrupamento = df.groupby('RAZAO_SOCIAL_EXIBIDORA').agg({
+    'PUBLICO': 'sum',  # Função de agregação 1
+    'TITULO_ORIGINAL': 'count'  # Função de agregação 2 (contagem de títulos)
+})
+
+# 3) Função condicional
+df['classificacao'] = df['PUBLICO'].apply(lambda x: 'Público Alto' if x > 50 else 'Público Baixo')
+
+# 4) Função de conversão
+df['PUBLICO'] = pd.to_numeric(df['PUBLICO'], errors='coerce')
+
+# 5) Função de data -> data para date time e pega o mês
+df['DATA_EXIBICAO'] = pd.to_datetime(df['DATA_EXIBICAO'], errors='coerce', format='%d/%m/%Y')
+df['mes'] = df['DATA_EXIBICAO'].dt.month
+
+# 6) Função de String -> nome da exibidora em maiuscula
+df['RAZAO_SOCIAL_EXIBIDORA_UPPER'] = df['RAZAO_SOCIAL_EXIBIDORA'].str.upper()
+
+
+print(df)
--- Sprint5/README.md
+++ Sprint5/README.md
@@ -1,22 +1,32 @@
 # Sprint 5 ⋆
 ## Sumário
-- ### [Conteúdos da Udemy]()
 - ### [Curso AWS Cloud Quest: Cloud Practitioner]()
-- ### [Exercícios propostos na Sprint 5]()
+- ### [Curso AWS Certified Cloud Practitioner]()
 - ### [Desafios propostos na Sprint 5]()
-- ### [Dificuldades encontradas](https://github.com/mayaramog/compassUOLmayara/tree/main/Sprint4#dificuldades-encontradas-1)
-- ### [Pontos a melhorar para a próxima Sprint](https://github.com/mayaramog/compassUOLmayara/tree/main/Sprint4#pontos-a-melhorar-para-a-pr%C3%B3xima-sprint-1)
-
-## Conteúdos da Udemy
+- ### [Dificuldades encontradas]()
+- ### [Pontos a melhorar para a próxima Sprint]()
 
 ## Curso AWS Cloud Quest: Cloud Practitioner
 - Link da certificação: https://www.credly.com/badges/11c486c2-e633-4484-b9f2-7b58ace1d735/public_url
 
 ![Certificação cloud quest](/Sprint5/Evidências/aws-cloud-quest-cloud-practitioner-mayara-goncalves.png)
-## Exercícios propostos na Sprint 5
+
+- [Evidências](/Sprint5/Evidências/) do processo de finalização do jogo da AWS Cloud Quest
+
+## Curso AWS Certified Cloud Practitioner
+
+Curso de preparação para a prova da AWS
+
+- [Certificado](/Sprint5/Certificados/18719_5_6046265_1727467355_AWS%20Skill%20Builder%20Course%20Completion%20Certificate.pdf)
 
 ## Desafios propostos na Sprint 5
+- Realizar consultas (6 consultas com algumas especificações em python) em um banco de dados escolhido pelo aluno
+
+- [consultas.py](/Sprint5/Desafio/consultas.py)
+- [Banco de dados escolhido](/Sprint5/Desafio/bilheteria-diaria-obras-por-exibidoras-2024-08-s04.csv)
 
 ## Dificuldades encontradas
+- Realizar as consultas em python, não pe uma linguagem que domino e me confundi ao fazer 6 consultas em uma.
 
 ## Pontor a melhorar para a próxima Sprint
+- Organização de atividades e tempo

e2b9aa68dde4337d86dd8a5e6ac7c9dfd39db90c
Mayara Marques <164480318+mayaramog@users.noreply.github.com>
2024-10-16 11:30:21 +0000

    atts sprint 5

--- Sprint5/Desafio/consultas.py
+++ Sprint5/Desafio/consultas.py
@@ -1,12 +1,38 @@
 import pandas as pd
+import boto3
+from io import StringIO
+from dotenv import load_dotenv
+import os
 
-# Carregar o arquivo CSV
-arquivo_csv = 'bilheteria-diaria-obras-por-exibidoras-2024-08-s04.csv'
-df = pd.read_csv(arquivo_csv, delimiter=';')  # Usando o delimitador correto (;)
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Acessar as variáveis de ambiente
+public_key = os.getenv('AWS_ACCESS_KEY_ID')
+secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+access_token = os.getenv('AWS_SESSION_TOKEN')
+bucket_name = os.getenv('BUCKET_NAME')
+
+# Configurar o cliente boto3 para acessar o S3
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=public_key,
+    aws_secret_access_key=secret_key,
+    aws_session_token=access_token
+)
+
+# Informações do bucket e do arquivo
+file_key = 'bilheteria-diaria-obras-por-exibidoras-2024-08-s04.csv'
+
+# Fazer o download do arquivo CSV a partir do S3
+obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+csv_data = obj['Body'].read().decode('utf-8')
+
+# Carregar o arquivo CSV em um DataFrame
+df = pd.read_csv(StringIO(csv_data), delimiter=';')
 
 # 1) Dois operadores lógicos
 consulta1 = df[(df['PUBLICO'] > 5) & (df['UF_SALA_COMPLEXO'] == 'BA')]
-
 
 # 2) Duas funções de agregação (soma e contagem)
 agrupamento = df.groupby('RAZAO_SOCIAL_EXIBIDORA').agg({
@@ -27,5 +53,5 @@ df['mes'] = df['DATA_EXIBICAO'].dt.month
 # 6) Função de String -> nome da exibidora em maiuscula
 df['RAZAO_SOCIAL_EXIBIDORA_UPPER'] = df['RAZAO_SOCIAL_EXIBIDORA'].str.upper()
 
-
+# Exibir o DataFrame modificado
 print(df)

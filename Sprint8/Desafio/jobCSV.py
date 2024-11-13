3af13bfdf1dc475889772f3184aca7cb93f4ba34
Mayara Marques <164480318+mayaramog@users.noreply.github.com>
2024-11-13 13:55:33 +0000

    ajuste na sprint 8

--- Sprint8/Desafio/jobCSV.py
+++ Sprint8/Desafio/jobCSV.py
@@ -5,7 +5,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from datetime import datetime
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import lit, col, when
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -22,6 +22,23 @@ output_path = "s3://desafio-sprint-6/Trusted/Local/CSV/Movies"
 
 # Leitura do arquivo CSV
 df = spark.read.option("delimiter", "|").csv(input_path, header=True)
+
+df = df.withColumnRenamed("titulopincipal", "tituloprincipal")
+
+# Tratamento de dados
+df = df.withColumn("tituloprincipal", when(col("tituloprincipal") != "\\N", col("tituloprincipal")).otherwise(""))
+df = df.withColumn("anolancamento", when((col("anolancamento") != "\\N") & (col("anolancamento") != ""), col("anolancamento").cast("int")).otherwise(None))
+df = df.withColumn("tempominutos", when((col("tempominutos") != "\\N") & (col("tempominutos") != ""), col("tempominutos").cast("int")).otherwise(None))
+df = df.withColumn("genero", when(col("genero") != "\\N", col("genero")).otherwise(""))
+df = df.withColumn("notamedia", when((col("notamedia") != "\\N") & (col("notamedia") != ""), col("notamedia").cast("double")).otherwise(None))
+df = df.withColumn("numerovotos", when((col("numerovotos") != "\\N") & (col("numerovotos") != ""), col("numerovotos").cast("int")).otherwise(None))
+df = df.withColumn("generoartista", when(col("generoartista") != "\\N", col("generoartista")).otherwise(""))
+df = df.withColumn("personagem", when(col("personagem") != "\\N", col("personagem")).otherwise(""))
+df = df.withColumn("nomeartista", when(col("nomeartista") != "\\N", col("nomeartista")).otherwise(""))
+df = df.withColumn("anonascimento", when((col("anonascimento") != "\\N") & (col("anonascimento") != ""), col("anonascimento").cast("int")).otherwise(None))
+df = df.withColumn("anofalecimento", when((col("anofalecimento") != "\\N") & (col("anofalecimento") != ""), col("anofalecimento").cast("int")).otherwise(None))
+df = df.withColumn("profissao", when(col("profissao") != "\\N", col("profissao")).otherwise(""))
+df = df.withColumn("titulosmaisconhecidos", when(col("titulosmaisconhecidos") != "\\N", col("titulosmaisconhecidos")).otherwise(""))
 
 # Adicionando uma coluna de data para particionamento
 current_date = datetime.now().strftime("%Y-%m-%d")
--- Sprint8/Desafio/jobJSON.py
+++ Sprint8/Desafio/jobJSON.py
@@ -6,7 +6,6 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from datetime import datetime
 from pyspark.sql.functions import lit
-from pyspark.sql.types import *
 
 # @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -21,52 +20,8 @@ job.init(args['JOB_NAME'], args)
 input_path = "s3://desafio-sprint-6/Raw/TMDB/JSON/2024/11/04/"
 output_path = "s3://desafio-sprint-6/Trusted/Local/JSON/TMDB"
 
-# Definindo o esquema detalhado dos dados
-schema = StructType([
-    StructField('adult', BooleanType()),
-    StructField('backdrop_path', StringType()),
-    StructField('belongs_to_collection', StringType()),
-    StructField('budget', IntegerType()),
-    StructField('genres', ArrayType(StructType([
-        StructField('id', IntegerType()),
-        StructField('name', StringType())
-    ]))),
-    StructField('homepage', StringType()),
-    StructField('id', IntegerType()),
-    StructField('imdb_id', StringType()),
-    StructField('original_language', StringType()),
-    StructField('original_title', StringType()),
-    StructField('overview', StringType()),
-    StructField('popularity', FloatType()),
-    StructField('poster_path', StringType()),
-    StructField('production_companies', ArrayType(StructType([
-        StructField('id', IntegerType()),
-        StructField('logo_path', StringType()),
-        StructField('name', StringType()),
-        StructField('origin_country', StringType())
-    ]))),
-    StructField('production_countries', ArrayType(StructType([
-        StructField('iso_3166_1', StringType()),
-        StructField('name', StringType())
-    ]))),
-    StructField('release_date', StringType()),
-    StructField('revenue', IntegerType()),
-    StructField('runtime', IntegerType()),
-    StructField('spoken_languages', ArrayType(StructType([
-        StructField('english_name', StringType()),
-        StructField('iso_639_1', StringType()),
-        StructField('name', StringType())
-    ]))),
-    StructField('status', StringType()),
-    StructField('tagline', StringType()),
-    StructField('title', StringType()),
-    StructField('video', BooleanType()),
-    StructField('vote_average', FloatType()),
-    StructField('vote_count', IntegerType())
-])
-
-# Leitura dos arquivos JSON com o esquema definido
-df = spark.read.schema(schema).json(input_path)
+# Leitura dos arquivos JSON sem definir o esquema
+df = spark.read.json(input_path)
 
 # Adicionando uma coluna de data para particionamento
 current_date = datetime.now().strftime("%Y-%m-%d")
